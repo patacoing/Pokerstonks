4ef593304470e0b8fb236d1588f90ee05a70dc35
patacoing <romain_lefebvre11@orange.fr>
2021-05-30 21:23:44 +0200

    ajout boutons check,fold,raise,1/2*pot,1*pot,2*pot,call + pot + mise + leurs fonctions associés -- PAS FONCTIONNEL (pas encore d'ajax)

--- javascript/main.js
+++ javascript/main.js
@@ -6,6 +6,23 @@ var ctx;
 var nbj = 3 ;//symbolique
 var size = 100; //largeur du rect
 
+
+//--------------------------Récupéré vià ajax--------------------------------------
+//info sur partie :
+var pot =0;
+var carteManche = new Array(); //retient les 5 cartes générées
+var miseJoueurAvant = 10;
+
+//info sur le joueur :
+var argent  =5;
+var couche = 0; //si vaut 1 ==> mettre les boutons d'actions en disabled
+var carteJoueur = new Array();
+var deltaMise = 0;
+var monTour = 0;  // si ==1 ==> je peux jouer sinon on disable les boutons 
+//----------------------------------------------------------------------------------
+
+
+
 var depart = 50;
 var taille = window.innerWidth-2*depart;
 var rajout = (taille)/nbj;
@@ -13,8 +30,8 @@ var rajoutPlateau = taille/5;
 var rajoutPerso = taille/2;
 var retenue = new Array();
 var image = new Array();
-var carteManche = new Array(); //retient les 5 cartes générées
-var carteJoueur = new Array();
+
+
 var tailleX = 135*0.75;
 var tailleY = 196*0.75;
 for(let i = 0; i < 52;i++)
@@ -25,11 +42,14 @@ for(let i = 0; i < 52;i++)
 
 function init(){
     c = document.getElementById("myCanvas");
-    c.width = window.innerWidth-2;
+    c.width = window.innerWidth-20;
     ctx = c.getContext("2d");
     drawJoueur(10);
     genPlateau();
     genPerso();
+ 
+
+
     
 }
 function drawJoueur(axeY){
@@ -61,10 +81,10 @@ function genPlateau()
         i++;
         drawCarte(230,alea,dist);
         dist += taille/5;
-        }
-         
+        }     
     }
 }
+
 function genPerso()
 {
     var alea;
@@ -75,7 +95,7 @@ function genPerso()
         alea = parseInt(Math.random()*52);
         if(checkCarte(alea))
         {
-        carteManche[i] = alea;
+        carteJoueur[i] = alea;
         drawCarte(400,alea,dist);
         dist += taille/2 ;
         i++;
@@ -99,4 +119,51 @@ function checkCarte(nb)
     }
     retenue[i+1] = nb; //on enregistre
     return true;
+}
+
+
+
+
+
+
+function check(){
+    //ne rien faire  : 
+    //seulement si la mise du joueur d'avant n'est pas plus grande que la mienne
+    if(miseJoueurAvant == mise){
+
+    }
+}
+
+function fold(){
+    //se coucher ==> tous nos boutons sont disabled jusqu'à la prochaine manche 
+    couche = 1;
+}
+
+function raise(){
+    //valider son choix de relance 
+    pot += deltaMise;
+    argent -= deltaMise;
+}
+
+function moitiePot(){
+    deltaMise = 1/2 * pot;
+    return;
+}
+
+function unPot(){
+    deltaMise = pot;
+    return;
+}
+
+function deuxPot(){
+    deltaMise = 2*pot;
+    return;
+}
+
+function call(){
+    //on met la même mise que le joueurs d'avant
+    deltaMise = (miseJoueurAvant - mise) 
+    mise += deltaMise;
+    pot += deltaMise;
+    argent -= mise;
 }
--- templates/partie.php
+++ templates/partie.php
@@ -12,8 +12,18 @@ $joueursDansPartie = listeUserDansPartie($idPartie);
 tprint($joueursDansPartie);
 ?>
 
+<input type="button" id="check" value="check" onclick="check()"/>
+<input type="button" id="fold" value="fold" onclick="fold()"/>
+<input type="button" id="raise" value="raise" onclick="raise()"/>
+<input type="button" id="moitiePot" value="1/2 pot" onclick="moitiePot()"/>
+<input type="button" id="unPot" value="1 * pot" onclick="unPot()"/>
+<input type="button" id="deuxPot" value="2 * pot" onclick="deuxPot()"/>
+<input type="button" id="call" value="call" onclick="call()"/>
+<div id="mise">mise : 0€</div>
+<div id="pot">pot : 0€</div>
 
-<canvas id="myCanvas" width="1500" height="1000" style="border:1px solid black;"> </canvas>
+
+<canvas id="myCanvas" width="1500" height="1000" style="border:1px solid black;"></canvas>
 <script type="text/javascript" src="javascript/carte.js"> </script>
 <script type="text/javascript" src="javascript/main.js"> </script>
 
