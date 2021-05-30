070e7e8672245c93a7a6a5de5c0808094af368de
Charles <charles25.bournonville@gmail.com>
2021-05-30 19:09:55 +0200

    carte aléa indépendantes et tout est gen

--- javascript/main.js
+++ javascript/main.js
@@ -9,8 +9,12 @@ var size = 100; //largeur du rect
 var depart = 50;
 var taille = 1400;
 var rajout = (taille)/nbj;
-var distance =   depart + (rajout - size)/2; 
+var rajoutPlateau = taille/5;
+var rajoutPerso = taille/2;
+var retenue = new Array();
 var image = new Array();
+var carteManche = new Array(); //retient les 5 cartes générées
+var carteJoueur = new Array();
 var tailleX = 135*0.75;
 var tailleY = 196*0.75;
 for(let i = 0; i < 52;i++)
@@ -23,12 +27,13 @@ function init(){
     c = document.getElementById("myCanvas");
     c.width = window.innerWidth-2;
     ctx = c.getContext("2d");
-    draw(10);
-    gencarte(210);
+    drawJoueur(10);
+    genPlateau();
+    genPerso();
     
 }
-function draw(axeY){
-    var dist = distance;
+function drawJoueur(axeY){
+    var dist =   depart + (rajout - size)/2;;
     ctx.beginPath();
     ctx.fillStyle = "green";
     ctx.fillRect(depart, axeY, taille, 800);
@@ -43,14 +48,55 @@ function draw(axeY){
     }
     
 }
-function gencarte(axeY){
-    var dist = distance;
-    for(let i = 0; i < 3;i++){
+function genPlateau()
+{
+    var alea;
+    var dist =   depart + (rajoutPlateau - size)/2;;
+    let i =0;
+    while(i < 5){
         alea = parseInt(Math.random()*52);
-        
-        ctx.beginPath();
-        ctx.drawImage(image[alea],dist,axeY,tailleX,tailleY);
-        ctx.stroke();
-        dist += rajout ;
+        if(checkCarte(alea))
+        {
+        carteManche[i] = alea;
+        i++;
+        drawCarte(230,alea,dist);
+        dist += taille/5;
+        }
+         
     }
+}
+function genPerso()
+{
+    var alea;
+    var dist = depart + (rajoutPerso - size)/2;
+    let i = 0;
+    while(i < 2)
+    {
+        alea = parseInt(Math.random()*52);
+        if(checkCarte(alea))
+        {
+        carteManche[i] = alea;
+        drawCarte(400,alea,dist);
+        dist += taille/2 ;
+        i++;
+        }
+         
+    }
+}
+function drawCarte(axeY,indice,dist){
+        ctx.beginPath();
+        ctx.drawImage(image[indice],dist,axeY,tailleX,tailleY);
+        ctx.stroke();
+        
+}
+function checkCarte(nb)
+{
+    for(var i = 0; i < retenue.length; i++){
+        if(retenue[i] == nb)
+        {
+            return false;
+        }
+    }
+    retenue[i+1] = nb; //on enregistre
+    return true;
 }
