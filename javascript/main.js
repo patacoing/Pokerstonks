2881a8343282db3761783da70ad0bde6a5255f6a
Charles <charles25.bournonville@gmail.com>
2021-05-30 18:39:28 +0200

    resize carte et canvas

--- javascript/main.js
+++ javascript/main.js
@@ -11,7 +11,8 @@ var taille = 1400;
 var rajout = (taille)/nbj;
 var distance =   depart + (rajout - size)/2; 
 var image = new Array();
-
+var tailleX = 135*0.75;
+var tailleY = 196*0.75;
 for(let i = 0; i < 52;i++)
 {
     image[i] = new Image();
@@ -30,7 +31,7 @@ function draw(axeY){
     var dist = distance;
     ctx.beginPath();
     ctx.fillStyle = "green";
-    ctx.fillRect(depart, axeY, taille, 300);
+    ctx.fillRect(depart, axeY, taille, 800);
     ctx.stroke();
     for(let i = 0; i < nbj;i++)
     {
@@ -48,7 +49,7 @@ function gencarte(axeY){
         alea = parseInt(Math.random()*52);
         
         ctx.beginPath();
-        ctx.drawImage(image[alea],dist,axeY);
+        ctx.drawImage(image[alea],dist,axeY,tailleX,tailleY);
         ctx.stroke();
         dist += rajout ;
     }
--- templates/partie.php
+++ templates/partie.php
@@ -13,7 +13,7 @@ tprint($joueursDansPartie);
 ?>
 
 
-<canvas id="myCanvas" width="1500" height="500" style="border:1px solid black;"> </canvas>
+<canvas id="myCanvas" width="1500" height="1000" style="border:1px solid black;"> </canvas>
 <script type="text/javascript" src="javascript/carte.js"> </script>
 <script type="text/javascript" src="javascript/main.js"> </script>
 
