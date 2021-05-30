aec4724d9a4da6219bae3f09457654e2f911c3fd
Charles <charles25.bournonville@gmail.com>
2021-05-30 22:00:35 +0200

    opti

--- javascript/main.js
+++ javascript/main.js
@@ -11,16 +11,21 @@ var taille = 1400;
 var rajout = (taille)/nbj;
 var rajoutPlateau = taille/5;
 var rajoutPerso = taille/2;
-var retenue = new Array();
+var tab = new Array();
+var tabbis = new Array();
 var image = new Array();
 var carteManche = new Array(); //retient les 5 cartes générées
 var carteJoueur = new Array();
 var tailleX = 135*0.75;
 var tailleY = 196*0.75;
+var Distrib = true;
 for(let i = 0; i < 52;i++)
 {
     image[i] = new Image();
     image[i].src = paquet[i].lien;
+    tab[i] = i;
+
+
 }
 
 function init(){
@@ -52,16 +57,14 @@ function genPlateau()
 {
     var alea;
     var dist =   depart + (rajoutPlateau - size)/2;;
-    let i =0;
-    while(i < 5){
-        alea = parseInt(Math.random()*52);
-        if(checkCarte(alea))
-        {
-        carteManche[i] = alea;
-        i++;
-        drawCarte(230,alea,dist);
+    for(let i = 0; i < 5;i++)
+    {
+        alea = parseInt(Math.random()*tab.length);
+        carteManche[i] = tab[alea];
+        drawCarte(230,tab[alea],dist);
         dist += taille/5;
-        }
+        tab.splice(alea,1);
+        
          
     }
 }
@@ -69,34 +72,34 @@ function genPerso()
 {
     var alea;
     var dist = depart + (rajoutPerso - size)/2;
-    let i = 0;
-    while(i < 2)
+    for(let i = 0; i < 2;i++)
     {
-        alea = parseInt(Math.random()*52);
-        if(checkCarte(alea))
-        {
-        carteManche[i] = alea;
-        drawCarte(400,alea,dist);
+        alea = parseInt(Math.random()*tab.length);
+        carteJoueur[i] = tab[alea];
+        drawCarte(400,tab[alea],dist);
+        console.log(tab);
+        tab.splice(alea,1);
+    
         dist += taille/2 ;
-        i++;
-        }
          
     }
 }
 function drawCarte(axeY,indice,dist){
+        console.log(indice);
         ctx.beginPath();
         ctx.drawImage(image[indice],dist,axeY,tailleX,tailleY);
         ctx.stroke();
         
 }
+/*
 function checkCarte(nb)
 {
-    for(var i = 0; i < retenue.length; i++){
-        if(retenue[i] == nb)
+    for(var i = 0; i < tab.length; i++){
+        if(tab[i] == nb)
         {
             return false;
         }
     }
-    retenue[i+1] = nb; //on enregistre
+    tab[i+1] = nb; //on enregistre
     return true;
-}
+}*/
